e9a88f33255e999b6ff8e840c5e1f4b22850e6e2
Eduardo Cardoso <eduardocardoszo@gmail.com>
2019-08-28 16:56:23 -0300

    Otimizaçao delete

--- src/mensagens/mensagens.controller.ts
+++ src/mensagens/mensagens.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Put, Delete, Param, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Get, Put, Delete, Param, Res, HttpStatus, Query } from '@nestjs/common';
 import { CreateMensagemDto } from './dto/create-mensagens-dto';
 import { updateExpression } from '@babel/types';
 import { MensagensService } from './mensagens.service';
@@ -7,46 +7,47 @@ import { response } from 'express';
 
 @Controller('mensagens')
 export class MensagensController {
-    constructor (private mensagemServices:MensagensService) {
+    constructor(private mensagemServices: MensagensService) {
     }
 
     @Post()
-    create (@Body() createMensagemDto: CreateMensagemDto, @Res() response) {
-        this.mensagemServices.createMensagem(createMensagemDto).then(mensagem =>{
+    create(@Body() createMensagemDto: CreateMensagemDto, @Res() response) {
+        this.mensagemServices.createMensagem(createMensagemDto).then(mensagem => {
             response.status(HttpStatus.CREATED).json(mensagem)
-        }).catch( () => {
-            response.status(HttpStatus.FORBIDDEN).json({mensagem: 'Erro na criacao da mensagem!'})
+        }).catch(() => {
+            response.status(HttpStatus.FORBIDDEN).json({ mensagem: 'Erro na criacao da mensagem!' })
         }
-        )}
-
-    @Get()
-    getAll(@Res() response){
-        this.mensagemServices.getAll().then(mensagemList => {
-            response.status(HttpStatus.OK).json(mensagemList);
-        }).catch( () => {
-            response.status(HttpStatus.FORBIDDEN).json({mensagem: 'Erro na recuperacao das mensagens!'})
-        }
-        )}
-
-    @Put(':id')
-    update (@Body() createMensagemDto: CreateMensagemDto, @Res() response, @Param('id') idMensagem){
-        this.mensagemServices.updateMensagem(idMensagem, createMensagemDto).then(mensagem =>{
-            response.status(HttpStatus.OK).json(mensagem)
-        }
-        ).catch( () => {
-            response.status(HttpStatus.FORBIDDEN).json({mensagem: 'Erro na atualizacao da mensagens!'})
-        }
-        )}
-
-    @Delete(':id')
-    delete(@Res() response, @Param('id') idMensagem){
-        this.mensagemServices.deleteMensagem(idMensagem).then(res => {
-             response.status(HttpStatus.OK).json(res)
-        }).catch( () => {
-            response.status(HttpStatus.FORBIDDEN).json({mensagem: 'Erro na remocao da mensagens!'})
-        })
+        )
     }
 
+    @Get()
+    getAll(@Res() response) {
+        this.mensagemServices.getAll().then(mensagemList => {
+            response.status(HttpStatus.OK).json(mensagemList);
+        }).catch(() => {
+            response.status(HttpStatus.FORBIDDEN).json({ mensagem: 'Erro na recuperacao das mensagens!' })
+        }
+        )
+    }
+
+    @Put(':id')
+    update(@Body() createMensagemDto: CreateMensagemDto, @Res() response, @Param('id') idMensagem) {
+        this.mensagemServices.updateMensagem(idMensagem, createMensagemDto).then(mensagem => {
+            response.status(HttpStatus.OK).json(mensagem)
+        }
+        ).catch(() => {
+            response.status(HttpStatus.FORBIDDEN).json({ mensagem: 'Erro na atualizacao da mensagens!' })
+        }
+        )
+    }
+
+    @Delete()
+    delete(@Res() response, @Query('id') idMensagem?, @Query('nick') nick?){ 
+            this.mensagemServices.deleteMensagem(idMensagem, nick).then(res => {
+                response.status(HttpStatus.OK).json(res)
+            }).catch(() => {
+                response.status(HttpStatus.FORBIDDEN).json({ mensagem: 'Erro na remocao da mensagens!' })
+            })
+
+    }
 }
-
-
--- src/mensagens/mensagens.service.ts
+++ src/mensagens/mensagens.service.ts
@@ -12,35 +12,39 @@ export class MensagensService {
     constructor(
         @InjectRepository(Mensagem)
         private readonly mensagemRepository: Repository<Mensagem>
-      ) {}
+    ) { }
 
     async getAll(): Promise<Mensagem[]> {
         return await this.mensagemRepository.find();
     }
 
-    async createMensagem(mensagemNova:CreateMensagemDto) : Promise<Mensagem> {
-        
+    async createMensagem(mensagemNova: CreateMensagemDto): Promise<Mensagem> {
+
         const nova = new Mensagem;
-        
+
         nova.nick = mensagemNova.nick;
         nova.mensagem = mensagemNova.mensagem;
-        
-        return this.mensagemRepository.save(nova);
-        }
 
-    async updateMensagem(idMensagem:number, mensagemAtualizar:CreateMensagemDto) : Promise<Mensagem> {
+        return this.mensagemRepository.save(nova);
+    }
+
+    async updateMensagem(idMensagem: number, mensagemAtualizar: CreateMensagemDto): Promise<Mensagem> {
         const mensagemUpdate = await this.mensagemRepository.findOne(idMensagem);
         mensagemUpdate.mensagem = mensagemAtualizar.mensagem;
         mensagemUpdate.nick = mensagemAtualizar.nick;
-    
-        return await this.mensagemRepository.save(mensagemUpdate);
-        }
 
-    async deleteMensagem (idMensagem:number): Promise<any>{
-        return await this.mensagemRepository.delete(idMensagem);        
+        return await this.mensagemRepository.save(mensagemUpdate);
     }
 
-   /*  async deleteMensagem2 (nick:string): Promise<any>{
-        return await this.mensagemRepository.createQueryBuilder().delete().where("nick= :nick", {nick: nick}).execute();        
-    } */
+    async deleteMensagem(idMensagem?: number, nick?: string): Promise<any> {
+        let res;
+        if (idMensagem) {
+             res = await this.mensagemRepository.delete(idMensagem)
+             console.log('Deletado por ID!')
+        } if (nick) {
+            res = await this.mensagemRepository.createQueryBuilder().delete().where("nick= :nick", { nick: nick }).execute()
+            console.log('Deletado por NICK!')
+            }
+        return res;
+    }
 }
